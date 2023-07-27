45ad454eb65739d7d95c4369313d2174cb100fbc
Bondaruk Liudmyla <ludabond123456@gmail.com>
2023-07-27 21:14:37 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,40 @@
+`use strict`;
+let stage = document.querySelector('.carousel-stage ');
+
+let prev = document.querySelector('.carousel-prev ');
+
+let next = document.querySelector('.carousel-next ');
+let carousel = document.querySelector('.carousel');
+let slide = document.querySelectorAll('.single-slide');
+let slideWidth = slide[0].clientWidth;
+console.log(slideWidth);
+let currentIndex = 0;
+let slidesNumber = slide.length - 1;
+
+function goToSlide(index) {
+    if (index < 0) {
+        index = slidesNumber;
+    } else if (index > slidesNumber) {
+        index = 0;
+    }
+
+    carousel.style.left = index * (-slideWidth);
+    currentIndex = index;
+}
+function slideToNext() {
+    goToSlide(currentIndex + 1);
+}
+
+function slideToPrev() {
+    goToSlide(currentIndex - 1);
+}
+
+function bindEvents() {
+    prev.addEventListener('click', slideToPrev);
+    next.addEventListener('click', slideToNext);
+}
+bindEvents();
+// function autorotate() {
+//     setInterval(slideToNext, 2000);
+// }
+// autorotate();
